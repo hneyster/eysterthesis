90d1c6f6567b7344c0a8b4de1aa4874f88c8aa71
hneyster <haroldeyster@gmail.com>
2016-11-06 14:54:44 -0800

    Now with log-transformd response

--- germ_timing_rstan.R
+++ germ_timing_rstan.R
@@ -3,17 +3,21 @@ rm(list=ls())
 options(stringsAsFactors = FALSE)
 options(shinystan.rstudio = TRUE)
 
+## some good references:
+# https://www.r-bloggers.com/bayesian-regression-with-stan-part-1-normal-regression/
+# http://m-clark.github.io/docs/IntroBayes.html 
+
 ##libraries
 library(rstan)
 library(shinystan)
-load("germs.Rdata") #cleaned and processed data 
+load("~/eysterthesis/germs.Rdata") #cleaned and processed data 
 
 #---------------Germination timing---------------------------------------------
 
 germs.y<-(subset(germs, 
                  germinated==1 & 
                    sp!="PLAMED" & 
-                   sp!="PLACOR"))           #just the data from germinated seeds 
+                   sp!="PLACOR"))    #just the data from seeds that germianted, and taking out the congenerics 
 
 ggplot(germs.y, aes(daysfromstart))+geom_histogram() #looking at a histogram of the data
 
@@ -21,25 +25,27 @@ ggplot(subset(germs.y, sp=="PLAMAJ" & temp==25.3), aes(daysfromstart))+geom_hist
 data<-subset(germs.y, sp="PLALAN")
 ## Setting up the data for the Stan model---------------------------------------
 
-n<-nrow(data)
+N<-nrow(data)
 y<-data$daysfromstart                    # dependent variable
 temp<-data$temp   # independent variable 
 strat<-data$strat
 dummy_variables <- model.matrix(~ origin, data = data)
 origin<-dummy_variables[,2]
-datax<-list(n=n, y=y, origin=origin, temp=temp, strat=strat)                  #bundling into a list 
+covariates<-matrix(c(origin, temp, strat), nrow=N)                  #covariate matrix
+X = cbind(intercept=1, covariates) #covariates + intercept 
+datax<-list(N=N, K=ncol(X), y=y, X=X)
 
 ## fitting into a Stan model -------------------------------------------------
 fit <- stan(file = "~/linreg.stan", data=datax, chains=10, iter=1000)
 
-print(fit, c("beta1", "beta2", "beta3", "sigma"))
+print(fit, c("beta" , "sigma"))
 
-stan_trace(fit, "beta1") #tests for mixing and convergence 
+stan_trace(fit, "beta[2]") #tests for mixing and convergence for temperature 
 stan_trace(fit, "sigma")
 modsims <- extract(fit)
 str(modsims)
-plot(modsims$beta1)
-pairs(fit, pars=c("beta1", "beta2", "beta3"))
+plot(modsims$beta)
+pairs(fit, pars=c("beta"))
 la<-extract(fit, permuted=TRUE) #shows parameters by iteration
 a<-extract(fit, permuted=FALSE) #shows parameters & chains & interations 
 m<-as.matrix(fit) # use the functions as.array or as.matrix with s3 objects 
@@ -48,6 +54,24 @@ print(fit, digits=3)
 #----launching shiny stan---------
 my_sso <- launch_shinystan(fit, rstudio = getOption("shinystan.rstudio"))
 
+#----R^2------------
+stanmodelcodeRsq = "
+.
+.
+.
+
+generated quantities {
+real rss;                
+real totalss;
+real<lower=0, upper=1> R2;                 
+vector[N] mu;
+
+mu <- X * beta;
+rss <- dot_self(y-mu);
+totalss <- dot_self(y-mean(y));
+R2 <- 1 - rss/totalss;
+}
+"
 
 
 
